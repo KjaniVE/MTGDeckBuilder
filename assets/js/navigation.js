d67bcd9b0c1e5b964e28758a261013855c9f1f38
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2024-12-11 09:57:38 +0100

    created the navigation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -3,16 +3,28 @@
 const NR_OF_BACKGROUNDS = 3;
 
 function initNavigation(){
-    
+    console.log("initNavigation");
+    document.querySelectorAll("button .nav").forEach((button) => {
+        button.addEventListener("click", navigate);
+    });
 }
 
 function navigate(e){
-
+    e.preventDefault();
+    const target = e.target;
+    navigateToPage(target);
 }
 
 function navigateToPage(targetId){
-
+    document.querySelectorAll(".page").forEach(page => {
+        console.log(targetId);
+        if (page.id === targetId){
+            page.classList.remove("hidden");
+        } else {
+            page.classList.add("hidden");
+        }
+    });
 }
 
-
+export { initNavigation, navigateToPage };
 // ## YOUR ADDED FUNCTIONS ##
