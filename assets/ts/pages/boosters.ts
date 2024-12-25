82b370db8c1e5e9bb1d1cc3b4cdd19b67a1bb3c6
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2024-12-25 21:07:26 +0100

    Add functions to initialize and render unopened boosters
    Introduce `setUnopenedBoosters` and `renderUnopenedBoosters` functions to populate and display booster packs dynamically. Refactor existing code for better clarity and fix typos in variable names and comments.

--- assets/ts/pages/boosters.ts
+++ assets/ts/pages/boosters.ts
@@ -1,9 +1,12 @@
-// _unopendBoosters: contains the list of boosterpacks, opened and/or unopened
+// _unopenedBoosters: contains the list of booster-packs, opened and/or unopened
 // for example: [0, 1, 2, 1, 3, 3] 
 // this means: first booster is open and has the version 0 image, second is not open and has the version image, third one is not open and has the version 2 image, ...
-// if you have an better/other solution here, you may implent your own!!!
+// if you have a better/other solution here, you may implement your own!!!
 
-let _unopendBoosters: number[] = [];
+import {getSubscribeForm} from "./subscribe.js";
+import {getRandomNumber} from "../utils.js";
+
+let _unopenedBoosters: number[] = [];
 
 const _MAX_BOOSTER_VERSIONS: number = 3;
 
@@ -11,8 +14,32 @@ const _MAX_BOOSTER_VERSIONS: number = 3;
 // ## GIVEN ##
 
 function initBoostersPage(): void {
-    console.log("Boosters page initialized");
+    const subscribeForm = getSubscribeForm();
+    setUnopenedBoosters(subscribeForm);
 }
 
 export {initBoostersPage};
 // ## YOUR ADDED FUNCTIONS ##
+
+function setUnopenedBoosters(subscribeForm: any) {
+    const boosterAmount: number = subscribeForm.boostersAmount;
+
+    for (let i = 0; i < boosterAmount; i++) {
+        _unopenedBoosters.push(getRandomNumber(_MAX_BOOSTER_VERSIONS, 1));
+    }
+
+    renderUnopenedBoosters(subscribeForm);
+}
+
+function renderUnopenedBoosters(subscribeForm: any) {
+    const $boosters : Element = document.querySelector("#unopenedBoosters")!;
+
+    $boosters.innerHTML = "";
+
+    _unopenedBoosters.forEach((booster, index) => {
+        $boosters.insertAdjacentHTML("beforeend", `
+        <li><img src="images/${subscribeForm.selectedSet}/booster_v${booster}.jpg" alt="booster" title="booster" data-index="${index}" data-open="0"></li>
+        `);
+    });
+
+}
